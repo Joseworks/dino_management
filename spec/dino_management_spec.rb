bc9e8d7e115c1eecb4373ed9bf3ba126e10f85a5
Jose C Fernandez <josefernandez@joseworks.org>
2025-04-11 14:49:02 -0400

    Add tests for empty dino list cases

--- spec/dino_management_spec.rb
+++ spec/dino_management_spec.rb
@@ -8,14 +8,27 @@ RSpec.describe DinoManagement do
   end
 
   describe 'Dino Management' do
-    let(:dino_data) do
-      [
-        { 'name' => 'DinoA', 'category' => 'herbivore', 'period' => 'Cretaceous', 'diet' => 'plants', 'age' => 100 },
-        { 'name' => 'DinoB', 'category' => 'carnivore', 'period' => 'Jurassic', 'diet' => 'meat', 'age' => 80 }
-      ]
+    let(:old_herbivore) do
+      { 'name' => 'DinoA', 'category' => 'herbivore', 'period' => 'Cretaceous', 'diet' => 'plants', 'age' => 100 }
     end
+    let(:healthy_carnivore) do
+      { 'name' => 'DinoB', 'category' => 'carnivore', 'period' => 'Jurassic', 'diet' => 'meat', 'age' => 80 }
+    end
+    let(:dino_data) { [old_herbivore, healthy_carnivore] }
 
     context 'when using the long and unoptimized method' do
+      describe 'with an empty dino list' do
+        let(:dino_data) { [] }
+
+        it 'returns an empty list' do
+          expect(described_class.run(dino_data)).to eq({ dinos: [], summary: {} })
+        end
+
+        it 'returns an empty summary' do
+          expect(described_class.run(dino_data)[:summary]).to eq({})
+        end
+      end
+
       describe 'dino health calculation' do
         subject(:result) { described_class.run(dino_data) }
 
