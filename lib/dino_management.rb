51828855396b2eb4afe64fa7c5c7a48cea66499a
Jose C Fernandez <josefernandez@joseworks.org>
2025-04-11 14:28:29 -0400

    Port the code from refactor_me to dino_management   - Add self to make run available

--- lib/dino_management.rb
+++ lib/dino_management.rb
@@ -1,8 +1,71 @@
 # frozen_string_literal: true
 
-require_relative "dino_management/version"
+# This is a poorly written code for the management of dinos.
+# The purpose of this code is to serve as a refactor test for
+# candidates applying for a software engineer position at our company.
+# We expect you to refactor it and turn it into an efficient
+# and maintainable code, following best practices. Fill in the Rspect test as well, modify it to your liking,
+# we do want to see some decent testing.
+# Please don't spend too much time on this, we know your time is valuable and we want to
+# make this fun but also allow you to show off your ruby skills :)
+#
+# Existing data: [
+#   { "name"=>"DinoA", "category"=>"herbivore", "period"=>"Cretaceous", "diet"=>"plants", "age"=>100 },
+#   { "name"=>"DinoB", "category"=>"carnivore", "period"=>"Jurassic", "diet"=>"meat", "age"=>80 }
+# ]
+#
+
+require_relative 'dino_management/version'
 
 module DinoManagement
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.run(dinos)
+    dinos.each do |d|
+      age = if d['age'].is_a?(Integer)
+              d['age']
+            else
+              d['age'].to_i
+            end
+
+      if age.positive?
+        if d['category'] == 'herbivore'
+          d['health'] = d['diet'] == 'plants' ? (100 - age) : (100 - age) / 2
+        elsif d['category'] == 'carnivore'
+          d['health'] = d['diet'] == 'meat' ? (100 - age) : (100 - age) / 2
+        end
+      else
+        d['health'] = 0
+      end
+
+      d['comment'] = if d['health'].positive?
+                       'Alive'
+                     else
+                       'Dead'
+                     end
+
+      d['age_metrics'] = if d['comment'] == 'Alive'
+                           if age > 1
+                             (age / 2).to_i
+                           else
+                             0
+                           end
+                         else
+                           0
+                         end
+    end
+
+    if dinos&.length&.positive?
+      a = dinos.group_by { |d| d['category'] }.map do |category, dino_list|
+        { category: category, count: dino_list.count }
+      end
+    end
+
+    f = {}
+    a.each do |category_metrics|
+      f[category_metrics[:category]] = category_metrics[:count]
+    end
+
+    { dinos: dinos, summary: f }
+  end
 end
--- spec/dino_management_spec.rb
+++ spec/dino_management_spec.rb
@@ -1,11 +1,44 @@
 # frozen_string_literal: true
 
+require 'pry'
+
 RSpec.describe DinoManagement do
-  it "has a version number" do
-    expect(DinoManagement::VERSION).not_to be nil
+  it 'has a version number' do
+    expect(DinoManagement::VERSION).not_to be_nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'Dino Management' do
+    let(:dino_data) do
+      [
+        { 'name' => 'DinoA', 'category' => 'herbivore', 'period' => 'Cretaceous', 'diet' => 'plants', 'age' => 100 },
+        { 'name' => 'DinoB', 'category' => 'carnivore', 'period' => 'Jurassic', 'diet' => 'meat', 'age' => 80 }
+      ]
+    end
+
+    context 'when using the long and unoptimized method' do
+      describe 'dino health calculation' do
+        it 'calculates dino health using age, category and diet' do
+          # Fill in expectations here
+        end
+      end
+
+      describe 'dino comment setting' do
+        it 'assigns appropriate comment based on health' do
+          # Fill in expectations here
+        end
+      end
+
+      describe 'dino age metric calculation' do
+        it 'computes age_metrics based on age and comment' do
+          # Fill in expectations here
+        end
+      end
+
+      describe 'dino category summary' do
+        it 'counts dinos by categories' do
+          # Fill in expectations here
+        end
+      end
+    end
   end
 end
