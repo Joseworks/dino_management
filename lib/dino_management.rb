edf43f7e6f189ee35a249557abc3afb6e8ade5f6
Jose C Fernandez <josefernandez@joseworks.org>
2025-04-14 10:42:08 -0400

    Rename variables

--- lib/dino_management.rb
+++ lib/dino_management.rb
@@ -7,49 +7,47 @@ module DinoManagement
 
   def self.run(dinos)
     return { dinos: [], summary: {} } if dinos.empty? || dinos.nil?
-    dinos.each do |d|
-      if d['age'] > 0
-        if d['category'] == 'herbivore'
-          d['health'] = d['diet'] == 'plants' ? (100 - d['age']) : (100 - d['age']) / 2
-        else
-          if d['category'] == 'carnivore'
-            d['health'] = d['diet'] == 'meat' ? (100 - d['age']) : (100 - d['age']) / 2
-          end
+    dinos.each do |dino|
+      if dino['age'] > 0
+        if dino['category'] == 'herbivore'
+          dino['health'] = dino['diet'] == 'plants' ? (100 - dino['age']) : (100 - dino['age']) / 2
+        elsif dino['category'] == 'carnivore'
+          dino['health'] = dino['diet'] == 'meat' ? (100 - dino['age']) : (100 - dino['age']) / 2
         end
       else
-        d['health'] = 0
+        dino['health'] = 0
       end
 
-      if d['health'] > 0
-        d['comment'] = 'Alive'
+      if dino['health'] > 0
+        dino['comment'] = 'Alive'
       else
-        d['comment'] = 'Dead'
+        dino['comment'] = 'Dead'
       end
     end
 
-    dinos.each do |d|
-      if d['comment'] == 'Alive'
-        if d['age'] > 1
-          d['age_metrics'] = (d['age'] / 2).to_i
+    dinos.each do |dino|
+      if dino['comment'] == 'Alive'
+        if dino['age'] > 1
+          dino['age_metrics'] = (dino['age'] / 2).to_i
         else
-          d['age_metrics'] = 0
+          dino['age_metrics'] = 0
         end
       else
-        d['age_metrics'] = 0
+        dino['age_metrics'] = 0
       end
     end
 
     if dinos && dinos.length > 0
-      a = dinos.group_by { |d| d['category'] }.map do |category, dino_list|
+      categories = dinos.group_by { |dino| dino['category'] }.map do |category, dino_list|
         { category: category, count: dino_list.count }
       end
     end
 
-    f = {}
-    a.each do |category_metrics|
-      f[category_metrics[:category]] = category_metrics[:count]
+    summary = {}
+    categories.each do |category_metrics|
+      summary[category_metrics[:category]] = category_metrics[:count]
     end
 
-    { dinos: dinos, summary: f }
+    { dinos: dinos, summary: summary }
   end
 end
